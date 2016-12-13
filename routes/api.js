bb002dbbc3d6b76c46cf1a294beb70323d3db23d
Nick D'hondt <nick.dhondt@student.howest.be>
2016-12-13 11:57:36 +0100

    routes schaffolding

--- routes/api.js
+++ routes/api.js
@@ -20,7 +20,69 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/userbyuid/:user', function (req, res, next) {
-    res.json({voorbeeld: 'dit is een voorbeeld: ' + req.params.user})
+    res.json({param: req.params.user})
+});
+
+router.post('/adduser', function (req, res, next) {
+    res.json({});
+});
+
+router.post('/updateuser', function (req, res, next) {
+    res.json({});
+});
+
+router.post('/updatehousehold', function (req, res, next) {
+    res.json({});
+});
+
+router.post('/addusertohousehold', function (req, res, next) {
+    res.json({});
+});
+
+router.get('/householdbyemail/:email', function (req, res, next) {
+    res.json({param: req.params.email})
+});
+
+router.post('/leavehousehold', function (req, res, next) {
+    res.json({});
+});
+
+router.post('/addhousehold', function (req, res, next) {
+    res.json({});
+});
+
+router.get('/taskstodobyhousehold/:household/:term?', function (req, res, next) {
+    if (req.params.term === undefined) req.params.term = 7;
+    res.json({params: req.params.household + ', ' + req.params.term})
+});
+
+router.post('/addtask', function (req, res, next) {
+    res.json({});
+});
+
+router.post('/updatetask', function (req, res, next) {
+    res.json({});
+});
+
+router.post('/finishtask', function (req, res, next) {
+    res.json({});
+});
+
+router.get('/deletetask/:task', function (req, res, next) {
+    res.json({param: req.params.task})
+});
+
+router.post('/addaward', function (req, res, next) {
+    res.json({});
+});
+
+router.get('/importtasks/:household/:assignusers?', function (req, res, next) {
+    if (req.params.assignusers === undefined) req.params.assignusers = false;
+    res.json({params: req.params.household + ', ' + req.params.assignusers})
+});
+
+router.post('/addtasks', function (req, res, next) {
+    res.json({});
 });
 
 module.exports = router;
