f998c2bddd365b1d1ceea1300ab30d6b958ee92e
Nick D'hondt <nick.dhondt@student.howest.be>
2016-12-26 14:00:14 +0100

    cors beter???

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ let admin = require("firebase-admin");
 let serviceAccount = require("./helpers/serviceAccount.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://kuisapp.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://kuisapp.firebaseio.com"
 });
 
 
@@ -30,13 +30,13 @@ app.set('view engine', 'ejs');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.all('/', function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
@@ -45,14 +45,14 @@ app.use('/', index);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
