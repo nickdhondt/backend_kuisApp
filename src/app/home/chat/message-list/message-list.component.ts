a7668799de9339ef06c5cfb3da4d70318066fd90
Nick D'hondt <nick.dhondt@student.howest.be>
2016-12-28 15:22:12 +0100

    socket port automatic

--- src/app/home/chat/message-list/message-list.component.ts
+++ src/app/home/chat/message-list/message-list.component.ts
@@ -14,7 +14,7 @@ export class MessageListComponent implements OnInit {
     socket = null;
 
     constructor() {
-        this.socket = io("http://localhost:3000");
+        this.socket = io(location.protocol+'//'+location.hostname+(location.port ? ':' + (location.port === '4200' ? "3000" : location.port) : ''));
         firebase.auth().currentUser.getToken().then(token => {
             this.socket.emit("subscribe", token);
         });
