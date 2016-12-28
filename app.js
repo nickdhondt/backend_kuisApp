b9c014039352575c577989717eca72a6e06f357d
BrentVanwildem <brent.vanwildemeersch@hotmail.be>
2016-12-29 00:02:57 +0100

    Chat messages

--- app.js
+++ app.js
@@ -21,23 +21,23 @@ let admin = require("firebase-admin");
 let serviceAccount = require("./helpers/serviceAccount.json");
 let http = require("http");
 let mongoose = require("mongoose");
-
-var uristring  =
-    process.env.MONGOLAB_URI||
-        process.env.MONGOHQ_URL||
-    "mongodb://admin:abc123@ds163667.mlab.com:63667/heroku_s3b0kwzb";
-mongoose.Promise = global.Promise;
-mongoose.connect(uristring,function (err,res) {
-    if(err){
-        console.log("Error connecting to : " + uristring + ". " + err);
-
-    }
-    else {
-        console.log("Succeed to connect to :" + uristring);
-    }
-
-});
-
+//
+// var uristring  =
+//     process.env.MONGOLAB_URI||
+//         process.env.MONGOHQ_URL||
+//     "mongodb://admin:abc123@ds163667.mlab.com:63667/heroku_s3b0kwzb";
+// mongoose.Promise = global.Promise;
+// mongoose.connect(uristring,function (err,res) {
+//     if(err){
+//         console.log("Error connecting to : " + uristring + ". " + err);
+//
+//     }
+//     else {
+//         console.log("Succeed to connect to :" + uristring);
+//     }
+//
+// });
+//
 
 
 
