84e3ab3852d79d8401ace04750f714fa2f4db2be
Nick D'hondt <nick.dhondt@student.howest.be>
2016-12-28 11:33:51 +0100

    chat service aangemaakt (werkt nog niet, alles staat nog in components)

--- src/app/home/chat/message-list/message-list.component.ts
+++ src/app/home/chat/message-list/message-list.component.ts
@@ -1,22 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../../../auth/services/auth.service";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-message-list',
-  templateUrl: './message-list.component.html',
-  styleUrls: ['./message-list.component.css']
+    selector: 'app-message-list',
+    templateUrl: './message-list.component.html',
+    styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
 
 
-
-
-
-
-
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
