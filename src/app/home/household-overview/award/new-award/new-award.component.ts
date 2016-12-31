60f4163b67c094b6afa67ea90c446c9b57699693
Nick D'hondt <nick.dhondt@student.howest.be>
2016-12-31 13:16:50 +0100

    award update

---
+++ src/app/home/household-overview/award/new-award/new-award.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit, Input, trigger, transition, style, animate, Output, EventEmitter} from '@angular/core';
+import {ApiService} from "../../../../../service/api.service";
+
+@Component({
+    selector: 'app-new-award',
+    templateUrl: './new-award.component.html',
+    styleUrls: ['./new-award.component.scss'],
+    animations: [
+        trigger('dialog', [
+            transition('void => *', [
+                style({transform: 'scale3d(.3, .3, .3)'}),
+                animate(100)
+            ]),
+            transition('* => void', [
+                animate(100, style({transform: 'scale3d(.0, .0, .0)'}))
+            ])
+        ])
+    ]
+})
+export class NewAwardComponent implements OnInit {
+    @Input() visible: boolean;
+    @Output() visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+    private name: String = "";
+    private description: String = "";
+
+    constructor(private apiService: ApiService) {
+    }
+
+    ngOnInit() {
+    }
+
+    close() {
+        this.visible = false;
+        this.visibleChange.emit(this.visible);
+    }
+
+    set() {
+        if (this.name !== "") {
+            this.apiService.setAward(this.name, this.description);
+            this.close();
+        }
+    }
+}
