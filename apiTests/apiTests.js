77ece02bde8defd9b23c19454013e11330dd0c9e
steven <steven.mollie@hotmail.com>
2017-01-04 22:32:09 +0100

    tests voor taken de gets

--- apiTests/apiTests.js
+++ apiTests/apiTests.js
@@ -1,6 +1,7 @@
 var assert = require("assert");
 var http = require('http');
 var should = require('should');
+var firebaseToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzNjhiYjg3NzVhYTI3YmI0MzlmNDYwNGZlNDk4Y2Y4YWYyMTQyNDEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20va3Vpc2FwcCIsIm5hbWUiOiJTdGV2ZW4gTW9sbGllIiwicGljdHVyZSI6Imh0dHBzOi8vc2NvbnRlbnQueHguZmJjZG4ubmV0L3YvbC90MS4wLTEvcDEwMHgxMDAvMTQzNTg2NDhfMTAyMDkxNjA5MzMyOTQyODBfNzU5MDI0MTczMjk5OTUwNTcyOF9uLmpwZz9vaD1lNTA0MjUzNWE4Y2NjZjViZTI1YTVkNGUwNDIyNWM5OSZvZT01OEQyMjNGNSIsImF1ZCI6Imt1aXNhcHAiLCJhdXRoX3RpbWUiOjE0ODMwMDkyOTcsInVzZXJfaWQiOiJHZllBMWhXYmtiZFU4dU93aENENFBXN3N5VVoyIiwic3ViIjoiR2ZZQTFoV2JrYmRVOHVPd2hDRDRQVzdzeVVaMiIsImlhdCI6MTQ4MzU2MjkxNSwiZXhwIjoxNDgzNTY2NTE1LCJlbWFpbCI6InN0ZXZlZGVtb2xsZUBob3RtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJmYWNlYm9vay5jb20iOlsiMTAyMDk2NzQ5NjM1MDQ3MTQiXSwiZW1haWwiOlsic3RldmVkZW1vbGxlQGhvdG1haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZmFjZWJvb2suY29tIn19.TBUGBYMItZ2SMZZmd114FyWsDU5yAP9eQ0RyShCb9uPsNd9foXlFvL0SSiGOaJDA1O7PHp_hiA5GpAYoxJeW3libFExRcRR_CP5J2eVmYRet_9X44S7QWBPBChd8FNSvIpmtJ05zDBq9AY7EWlj_Prtt7a30XMgTKLC-fp-vqRgOjX38sbUj5bDX6gA8RHSzPCRyW_Rh4jxftQG9O8kRNlE69yetMWI3C11RoxZqFqt5oidocezklKnrm2_hn1hXyZKoDNI5kIXUpk0QfVduB7LVgREno1z7uDDOOWCcWbhtkH2wQPjETsoVONSYp41h41NIq_GHRciq87PyXC3QOw';
 
 describe("user tests", function () {
     var lengthOfObject = function (obj) {
@@ -17,7 +18,7 @@ describe("user tests", function () {
             port: process.env.PORT || 3000,
             path: '/api/userlimited',
             method: 'GET',
-            headers: { 'Firebase-ID-Token': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzNjhiYjg3NzVhYTI3YmI0MzlmNDYwNGZlNDk4Y2Y4YWYyMTQyNDEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20va3Vpc2FwcCIsIm5hbWUiOiJTdGV2ZW4gTW9sbGllIiwicGljdHVyZSI6Imh0dHBzOi8vc2NvbnRlbnQueHguZmJjZG4ubmV0L3YvbC90MS4wLTEvcDEwMHgxMDAvMTQzNTg2NDhfMTAyMDkxNjA5MzMyOTQyODBfNzU5MDI0MTczMjk5OTUwNTcyOF9uLmpwZz9vaD1lNTA0MjUzNWE4Y2NjZjViZTI1YTVkNGUwNDIyNWM5OSZvZT01OEQyMjNGNSIsImF1ZCI6Imt1aXNhcHAiLCJhdXRoX3RpbWUiOjE0ODMwMDkyOTcsInVzZXJfaWQiOiJHZllBMWhXYmtiZFU4dU93aENENFBXN3N5VVoyIiwic3ViIjoiR2ZZQTFoV2JrYmRVOHVPd2hDRDRQVzdzeVVaMiIsImlhdCI6MTQ4MzU1NzcwNCwiZXhwIjoxNDgzNTYxMzA0LCJlbWFpbCI6InN0ZXZlZGVtb2xsZUBob3RtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJmYWNlYm9vay5jb20iOlsiMTAyMDk2NzQ5NjM1MDQ3MTQiXSwiZW1haWwiOlsic3RldmVkZW1vbGxlQGhvdG1haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZmFjZWJvb2suY29tIn19.cAszHOdHJn2OChu1oQhHwjay4adzHeaLAoQpukDaWEfv45Xwx-23HBOq7yY_2NOpe3PVCnTb0jdV28bwEPCCiLzctxq0IRoo-CuUxJb5jl6NUc2x8-NbYvhcurHQZ06WjDiFUJtvOkFn1SwVRvWAWSxEE2ljhKO1i6PhNYNqKhoEnxxfu9gn8BRjI5P0cCbZRrvlguCtYeIWUXlueyMCkPBMGVA8tvNPOcomO9jKrMNk2XqpiZuMwTan1B822lEyHq9K5nCWOsYpMM9_giaWexU_b45inXiem_u90XoLYMdZkB0upSjGJGD2ysOQ2y5WN9OVf_r2BBVR8YUh3L025g'}
+            headers: { 'Firebase-ID-Token': firebaseToken}
         };
 
         var req = http.request(opts, function (res) {
@@ -48,7 +49,7 @@ describe("user tests", function () {
             port: process.env.PORT || 3000,
             path: '/api/userbyuid/GfYA1hWbkbdU8uOwhCD4PW7syUZ2',
             method: 'GET',
-            headers: { 'Firebase-ID-Token': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzNjhiYjg3NzVhYTI3YmI0MzlmNDYwNGZlNDk4Y2Y4YWYyMTQyNDEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20va3Vpc2FwcCIsIm5hbWUiOiJTdGV2ZW4gTW9sbGllIiwicGljdHVyZSI6Imh0dHBzOi8vc2NvbnRlbnQueHguZmJjZG4ubmV0L3YvbC90MS4wLTEvcDEwMHgxMDAvMTQzNTg2NDhfMTAyMDkxNjA5MzMyOTQyODBfNzU5MDI0MTczMjk5OTUwNTcyOF9uLmpwZz9vaD1lNTA0MjUzNWE4Y2NjZjViZTI1YTVkNGUwNDIyNWM5OSZvZT01OEQyMjNGNSIsImF1ZCI6Imt1aXNhcHAiLCJhdXRoX3RpbWUiOjE0ODMwMDkyOTcsInVzZXJfaWQiOiJHZllBMWhXYmtiZFU4dU93aENENFBXN3N5VVoyIiwic3ViIjoiR2ZZQTFoV2JrYmRVOHVPd2hDRDRQVzdzeVVaMiIsImlhdCI6MTQ4MzU1NzcwNCwiZXhwIjoxNDgzNTYxMzA0LCJlbWFpbCI6InN0ZXZlZGVtb2xsZUBob3RtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJmYWNlYm9vay5jb20iOlsiMTAyMDk2NzQ5NjM1MDQ3MTQiXSwiZW1haWwiOlsic3RldmVkZW1vbGxlQGhvdG1haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZmFjZWJvb2suY29tIn19.cAszHOdHJn2OChu1oQhHwjay4adzHeaLAoQpukDaWEfv45Xwx-23HBOq7yY_2NOpe3PVCnTb0jdV28bwEPCCiLzctxq0IRoo-CuUxJb5jl6NUc2x8-NbYvhcurHQZ06WjDiFUJtvOkFn1SwVRvWAWSxEE2ljhKO1i6PhNYNqKhoEnxxfu9gn8BRjI5P0cCbZRrvlguCtYeIWUXlueyMCkPBMGVA8tvNPOcomO9jKrMNk2XqpiZuMwTan1B822lEyHq9K5nCWOsYpMM9_giaWexU_b45inXiem_u90XoLYMdZkB0upSjGJGD2ysOQ2y5WN9OVf_r2BBVR8YUh3L025g'}
+            headers: { 'Firebase-ID-Token': firebaseToken}
         };
 
         var req = http.request(opts, function (res) {
@@ -90,8 +91,7 @@ describe("household tests", function () {
             port: process.env.PORT || 3000,
             path: '/api/householdbyemail/mordicus_87@hotmail.com',
             method: 'GET',
-            headers: { 'Firebase-ID-Token': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzNjhiYjg3NzVhYTI3YmI0MzlmNDYwNGZlNDk4Y2Y4YWYyMTQyNDEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20va3Vpc2FwcCIsIm5hbWUiOiJTdGV2ZW4gTW9sbGllIiwicGljdHVyZSI6Imh0dHBzOi8vc2NvbnRlbnQueHguZmJjZG4ubmV0L3YvbC90MS4wLTEvcDEwMHgxMDAvMTQzNTg2NDhfMTAyMDkxNjA5MzMyOTQyODBfNzU5MDI0MTczMjk5OTUwNTcyOF9uLmpwZz9vaD1lNTA0MjUzNWE4Y2NjZjViZTI1YTVkNGUwNDIyNWM5OSZvZT01OEQyMjNGNSIsImF1ZCI6Imt1aXNhcHAiLCJhdXRoX3RpbWUiOjE0ODMwMDkyOTcsInVzZXJfaWQiOiJHZllBMWhXYmtiZFU4dU93aENENFBXN3N5VVoyIiwic3ViIjoiR2ZZQTFoV2JrYmRVOHVPd2hDRDRQVzdzeVVaMiIsImlhdCI6MTQ4MzU1NzcwNCwiZXhwIjoxNDgzNTYxMzA0LCJlbWFpbCI6InN0ZXZlZGVtb2xsZUBob3RtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJmYWNlYm9vay5jb20iOlsiMTAyMDk2NzQ5NjM1MDQ3MTQiXSwiZW1haWwiOlsic3RldmVkZW1vbGxlQGhvdG1haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZmFjZWJvb2suY29tIn19.cAszHOdHJn2OChu1oQhHwjay4adzHeaLAoQpukDaWEfv45Xwx-23HBOq7yY_2NOpe3PVCnTb0jdV28bwEPCCiLzctxq0IRoo-CuUxJb5jl6NUc2x8-NbYvhcurHQZ06WjDiFUJtvOkFn1SwVRvWAWSxEE2ljhKO1i6PhNYNqKhoEnxxfu9gn8BRjI5P0cCbZRrvlguCtYeIWUXlueyMCkPBMGVA8tvNPOcomO9jKrMNk2XqpiZuMwTan1B822lEyHq9K5nCWOsYpMM9_giaWexU_b45inXiem_u90XoLYMdZkB0upSjGJGD2ysOQ2y5WN9OVf_r2BBVR8YUh3L025g'
-            }
+            headers: { 'Firebase-ID-Token': firebaseToken}
         };
 
         var req = http.request(opts, function (res) {
@@ -115,13 +115,14 @@ describe("household tests", function () {
         });
         req.end();
     });
-    it('should return a household by uid', function (done) {
+
+    it('should get a household by email', function (done) {
         var opts = {
             host: '127.0.0.1',
             port: process.env.PORT || 3000,
             path: '/api/household',
             method: 'GET',
-            headers: { 'Firebase-ID-Token': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzNjhiYjg3NzVhYTI3YmI0MzlmNDYwNGZlNDk4Y2Y4YWYyMTQyNDEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20va3Vpc2FwcCIsIm5hbWUiOiJTdGV2ZW4gTW9sbGllIiwicGljdHVyZSI6Imh0dHBzOi8vc2NvbnRlbnQueHguZmJjZG4ubmV0L3YvbC90MS4wLTEvcDEwMHgxMDAvMTQzNTg2NDhfMTAyMDkxNjA5MzMyOTQyODBfNzU5MDI0MTczMjk5OTUwNTcyOF9uLmpwZz9vaD1lNTA0MjUzNWE4Y2NjZjViZTI1YTVkNGUwNDIyNWM5OSZvZT01OEQyMjNGNSIsImF1ZCI6Imt1aXNhcHAiLCJhdXRoX3RpbWUiOjE0ODMwMDkyOTcsInVzZXJfaWQiOiJHZllBMWhXYmtiZFU4dU93aENENFBXN3N5VVoyIiwic3ViIjoiR2ZZQTFoV2JrYmRVOHVPd2hDRDRQVzdzeVVaMiIsImlhdCI6MTQ4MzU2MjkxNSwiZXhwIjoxNDgzNTY2NTE1LCJlbWFpbCI6InN0ZXZlZGVtb2xsZUBob3RtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJmYWNlYm9vay5jb20iOlsiMTAyMDk2NzQ5NjM1MDQ3MTQiXSwiZW1haWwiOlsic3RldmVkZW1vbGxlQGhvdG1haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZmFjZWJvb2suY29tIn19.TBUGBYMItZ2SMZZmd114FyWsDU5yAP9eQ0RyShCb9uPsNd9foXlFvL0SSiGOaJDA1O7PHp_hiA5GpAYoxJeW3libFExRcRR_CP5J2eVmYRet_9X44S7QWBPBChd8FNSvIpmtJ05zDBq9AY7EWlj_Prtt7a30XMgTKLC-fp-vqRgOjX38sbUj5bDX6gA8RHSzPCRyW_Rh4jxftQG9O8kRNlE69yetMWI3C11RoxZqFqt5oidocezklKnrm2_hn1hXyZKoDNI5kIXUpk0QfVduB7LVgREno1z7uDDOOWCcWbhtkH2wQPjETsoVONSYp41h41NIq_GHRciq87PyXC3QOw'}
+            headers: { 'Firebase-ID-Token': firebaseToken}
         };
 
         var req = http.request(opts, function (res) {
@@ -145,6 +146,7 @@ describe("household tests", function () {
         });
         req.end();
     });
+
 });
 
 describe("task tests", function () {
@@ -160,9 +162,102 @@ describe("task tests", function () {
         var opts = {
             host: '127.0.0.1',
             port: process.env.PORT || 3000,
-            path: '/api/household',
+            path: '/api/taskstodobyhousehold/37',
             method: 'GET',
-            headers: { 'Firebase-ID-Token': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzNjhiYjg3NzVhYTI3YmI0MzlmNDYwNGZlNDk4Y2Y4YWYyMTQyNDEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20va3Vpc2FwcCIsIm5hbWUiOiJTdGV2ZW4gTW9sbGllIiwicGljdHVyZSI6Imh0dHBzOi8vc2NvbnRlbnQueHguZmJjZG4ubmV0L3YvbC90MS4wLTEvcDEwMHgxMDAvMTQzNTg2NDhfMTAyMDkxNjA5MzMyOTQyODBfNzU5MDI0MTczMjk5OTUwNTcyOF9uLmpwZz9vaD1lNTA0MjUzNWE4Y2NjZjViZTI1YTVkNGUwNDIyNWM5OSZvZT01OEQyMjNGNSIsImF1ZCI6Imt1aXNhcHAiLCJhdXRoX3RpbWUiOjE0ODMwMDkyOTcsInVzZXJfaWQiOiJHZllBMWhXYmtiZFU4dU93aENENFBXN3N5VVoyIiwic3ViIjoiR2ZZQTFoV2JrYmRVOHVPd2hDRDRQVzdzeVVaMiIsImlhdCI6MTQ4MzU2MjkxNSwiZXhwIjoxNDgzNTY2NTE1LCJlbWFpbCI6InN0ZXZlZGVtb2xsZUBob3RtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJmYWNlYm9vay5jb20iOlsiMTAyMDk2NzQ5NjM1MDQ3MTQiXSwiZW1haWwiOlsic3RldmVkZW1vbGxlQGhvdG1haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZmFjZWJvb2suY29tIn19.TBUGBYMItZ2SMZZmd114FyWsDU5yAP9eQ0RyShCb9uPsNd9foXlFvL0SSiGOaJDA1O7PHp_hiA5GpAYoxJeW3libFExRcRR_CP5J2eVmYRet_9X44S7QWBPBChd8FNSvIpmtJ05zDBq9AY7EWlj_Prtt7a30XMgTKLC-fp-vqRgOjX38sbUj5bDX6gA8RHSzPCRyW_Rh4jxftQG9O8kRNlE69yetMWI3C11RoxZqFqt5oidocezklKnrm2_hn1hXyZKoDNI5kIXUpk0QfVduB7LVgREno1z7uDDOOWCcWbhtkH2wQPjETsoVONSYp41h41NIq_GHRciq87PyXC3QOw'}
+            headers: { 'Firebase-ID-Token': firebaseToken}
+        };
+
+        var req = http.request(opts, function (res) {
+            res.setEncoding('utf8'); //comment
+            var result = "";
+            res.on('data', function (d) {
+                console.log(d);
+                result += d;
+            });
+            res.on('end', function (err) {
+                assert.equal(err,null);
+                assert.equal(res.statusCode, 200);
+                var data = JSON.parse(result);
+                assert.notEqual(lengthOfObject(data),0);
+                done();
+
+                if(err){
+                    throw err;
+                }
+            })
+        });
+        req.end();
+    });
+
+    it('should return the task for a user', function (done) {
+        var opts = {
+            host: '127.0.0.1',
+            port: process.env.PORT || 3000,
+            path: '/api/tasksbytoken',
+            method: 'GET',
+            headers: { 'Firebase-ID-Token': firebaseToken}
+        };
+
+        var req = http.request(opts, function (res) {
+            res.setEncoding('utf8'); //comment
+            var result = "";
+            res.on('data', function (d) {
+                console.log(d);
+                result += d;
+            });
+            res.on('end', function (err) {
+                assert.equal(err,null);
+                assert.equal(res.statusCode, 200);
+                var data = JSON.parse(result);
+                assert.notEqual(lengthOfObject(data),0);
+                done();
+
+                if(err){
+                    throw err;
+                }
+            })
+        });
+        req.end();
+    });
+
+    it('should delete a task', function (done) {
+        var opts = {
+            host: '127.0.0.1',
+            port: process.env.PORT || 3000,
+            path: '/api/deletetask/2941',
+            method: 'GET',
+            headers: { 'Firebase-ID-Token': firebaseToken }
+            };
+
+        var req = http.request(opts, function (res) {
+            res.setEncoding('utf8'); //comment
+            var result = "";
+            res.on('data', function (d) {
+                console.log(d);
+                result += d;
+            });
+            res.on('end', function (err) {
+                assert.equal(err,null);
+                assert.equal(res.statusCode, 200);
+                var data = JSON.parse(result);
+                assert.notEqual(lengthOfObject(data),0);
+                done();
+
+                if(err){
+                    throw err;
+                }
+            })
+        });
+        req.end();
+    });
+
+    it('should import all tasks', function (done) {
+        var opts = {
+            host: '127.0.0.1',
+            port: process.env.PORT || 3000,
+            path: '/api/importtasks/37/true',
+            method: 'GET',
+            headers: { 'Firebase-ID-Token': firebaseToken}
         };
 
         var req = http.request(opts, function (res) {
