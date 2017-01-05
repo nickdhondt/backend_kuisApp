41c2412e90f4836c4339a8a507d3656837ff57b1
Bart <bart.delrue@student.howest.be>
2017-01-05 14:44:08 +0100

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,6 @@ import {ApiService} from "../service/api.service";
 import {SocketService} from "../service/socket.service";
 import {UpdateTaskListService} from "../service/update-task-list.service";
 import {UpdateHouseholdOverviewService} from "../service/update-household-overview.service";
-import {enableProdMode} from '@angular/core';
 
 const myFirebaseConfig = {
     apiKey: "AIzaSyB_BRv8vUg4D0njciLkTNqGBEfZM4cNVlQ",
