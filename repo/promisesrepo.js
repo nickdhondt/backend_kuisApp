920421ff9fa911dbe57c8e9ec7bcf6e19292f8b7
Bart <bart.delrue@student.howest.be>
2017-01-07 15:37:11 +0100

    gzip

--- repo/promisesrepo.js
+++ repo/promisesrepo.js
@@ -177,6 +177,8 @@ let getUserPromises = (()=> {
                             if (stats[0]._id) wonby = findwinner(stats[0]._id, userwithhousehold.household.users);
                             else wonby = findwinners(stats[0].users, userwithhousehold.household.users);
 
+                            let mostwon = mostAwards._id ? this.findwinner(mostAwards._id, userwithhousehold.household.users) : "it's mostly a draw";
+
                             let result = {};
                             result.mostAwardsWon = mostAwards._id || "it's mostly a draw";
                             result.countFinishedAwards = total;
